cbd6adde48abb9df130d06466fd684245905943f
ryanou97 <zhonglinou.1997@gmail.com>
2023-11-03 16:11:32 +0800

    v 1.0
    add demo.png

--- license.py
+++ license.py
@@ -1,27 +1,45 @@
 import cv2
 import module as m     # 匯入自訂模組
 
-capture = cv2.VideoCapture(0)   # 建立攝影機物件
-if capture.isOpened():          
-    while True:
-        sucess, img = capture.read()        # 讀取影像
-        if sucess:
-            cv2.imshow('Frame', img)        # 顯示影像
+
+if __name__ == '__main__':
+    
+    capture = cv2.VideoCapture(0)   # 建立攝影機物件
+    
+    
+    if capture.isOpened():          # return True 為開啟成功  
+        
+        try:
+            while True:
+                sucess, img = capture.read()        # 讀取影像
+                if sucess:
+                    cv2.imshow('Frame', img)        # 顯示影像
+               
             
-        k = cv2.waitKey(100)                # 等待按鍵輸入
-        if k == ord('s') or k == ord('S'):  # 按下 S(s)
-            cv2.imwrite('shot.jpg', img)    # 儲存影像
-            text = m.get_license(img)         # 進行車牌辨識
-            print('車牌:', text)
-            
-        if k == ord('q') or k == ord('Q'):  # 按下 Q(q) 結束迴圈
-            print('exit')
-            cv2.destroyAllWindows()         # 關閉視窗
-            capture.release()               # 關閉攝影機
-            break
-else:
-    print('開啟攝影機失敗')
+    
+                # 等待鍵盤輸入
+                k = cv2.waitKey(100)                
+                
+                # 按下 'S' or 's' > 畫面儲存 > 進行車牌辨識
+                if k == ord('s') or k == ord('S'):
+                    cv2.imwrite('shot.jpg', img)    
+                    text = m.get_license(img)         
+                    print('車牌:', text)
+                    
+                # 按下 'Q' or 'q' > 關閉畫面、攝影機 > break
+                if k == ord('q') or k == ord('Q'): 
+                    print('結束辨識')
+                    cv2.destroyAllWindows()   
+                    capture.release()       
+                    break
+        except:
+            print("功能出錯")
+    
+    else:
+        
+        print('開啟攝影機失敗')
 
  
+
 
 
--- module.py
+++ module.py
@@ -9,31 +9,46 @@ import time
 import re
 import API_info
 
+"""    此處需更改    """
 base = API_info.base
 key = API_info.key
+""""""""""""""""""""""""
+
 recog_url = f'{base}/recognizeText?mode=Printed'
-headers = {'Ocp-Apim-Subscription-Key': key}                            # 查看結果的請求標頭
-headers_stream = {'Ocp-Apim-Subscription-Key': key,                     # 辨識的請求標頭
+headers = {'Ocp-Apim-Subscription-Key': key}                            # 查看結果的請求標header
+headers_stream = {'Ocp-Apim-Subscription-Key': key,                     # 辨識的請求header
                   'Content-Type': 'application/octet-stream'}
 
 
+
 def get_license(img):
-    img_encode = cv2.imencode('.jpg', img)[1]       # 將 img 編碼為 JPEG 格式，[1]返回資料, [0]返回是否成功
-    img_bytes = img_encode.tobytes()                # 再將資料轉為 bytes, 此即為要傳送的資料
-    r1 = requests.post(recog_url,            # 發出 POST 
-                              headers = headers_stream, 
-                              data = img_bytes)
-    if r1.status_code != 202:                # 202 代表接受請求
+    # 將 img 編碼為 JPEG 格式，再將資料轉為 bytes, 此即為要傳送的資料
+    # [1]返回資料, [0]返回是否成功
+    img_bytes = cv2.imencode('.jpg', img)[1].tobytes()
+    
+    
+    
+    # 發出 POST請求
+    r1 = requests.post(recog_url, headers = headers_stream, data = img_bytes)
+    
+    if r1.status_code != 202:     # 202 代表接受請求
         print(r1.json())
         return '請求失敗'
-    #--↓↓辨識請求成功↓↓--#
+    
+    
+    
+    # 辨識請求成功
     result_url = r1.headers['Operation-Location'] # 取得查看結果的請求路徑
     r2 = requests.get(result_url, headers = headers) # 發出 GET 請求
+    
     while r2.status_code == 200 and r2.json()['status'] != 'Succeeded':
-        r2 = requests.get(result_url, headers = headers)    # 繼續發出 GET
+        r2 = requests.get(result_url, headers = headers)    # 持續發出 GET
         time.sleep(0.5)
         print('status: ', r2.json()['status'])      # 顯示辨識狀態
-    #--↓↓辨識完成↓↓--#
+    
+        
+        
+    # 完成辨識
     carcard = ''  # 紀錄車牌
     lines = r2.json()['recognitionResult']['lines']
     for i in range(len(lines)):
@@ -42,5 +57,11 @@ def get_license(img):
         if m != None:   # 匹配成功
             carcard = m.group()
             return carcard
+        
+        
     if carcard == '':   # 無匹配結果
         return '找不到車牌'
+    
+    
+    
+    
